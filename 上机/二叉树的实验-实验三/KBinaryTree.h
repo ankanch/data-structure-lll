4d3bde724cb2d621f18058c163f1f0955e7810ef
Kanch <kanchisme@gmail.com>
2016-11-29 21:41:58 +0800

    修改代码结构

--- 上机/二叉树的实验-实验三/KBinaryTree.h
+++ 上机/二叉树的实验-实验三/KBinaryTree.h
@@ -3,12 +3,11 @@
 #include<queue>
 
 using namespace std;
-
+#define STACK_INIT_SIZE 100
+#define STACKINCREMENT 10
 #define OVERFLOW -1
 #define ERROR 0
 #define OK 1
-
-//二叉树定义
 typedef int Status; 
 typedef char TElemType;
 typedef struct BiTNode{
@@ -16,29 +15,20 @@ typedef struct BiTNode{
     struct BiTNode *lchild,*rchild;
 }BiTNode,*BiTree;
 
-Status CreateBiTree(BiTree &T);  //先序建立二叉树
+Status CreateBiTree(BiTree &T);
 Status PrintElement(TElemType &e);
 Status DestoryBiTree(BiTree &T);
-
-
-
-//二叉树遍历(递归)
 Status PreOrderTraverse(BiTree &T,Status(*Visit)(TElemType &e));
 Status InOrderTraverse(BiTree &T,Status(*Visit)(TElemType &e));
 Status PostOrderTraverse(BiTree &T,Status(*Visit)(TElemType &e));
 Status LevelTraverse(BiTree &T,Status(*Visit)(TElemType &e));
 int GetBiHeight(BiTree &T);  
 Status PrintSpecficLevel(BiTree &T,int level,Status(*Visit)(TElemType &e));
-//非递归
 Status InOrderTraverseNoRecursion(BiTree &T,Status(*Visit)(TElemType &e));
 Status PreOrderTraverseNoRecursion(BiTree &T,Status(*Visit)(TElemType &e));
-
-
+Status PreOrderTraverseNoRecursion(BiTree &T,Status(*Visit)(TElemType &e))
 //相关栈===============【为了非递归遍历】==================
 //由于g++版本问题，导致直接include 之前的KStack.h会导致重定义。故将必要代码拷贝到这里
-#define STACK_INIT_SIZE 100
-#define STACKINCREMENT 10
-
 typedef int Status;
 typedef BiTree SElemType;
 typedef struct{
@@ -46,7 +36,6 @@ typedef struct{
     SElemType *top;
     int stacksize;
 }SqStack;
-
 Status InitStack(SqStack &s);
 Status StackEmpty(SqStack s);
 Status GetTop(SqStack s,SElemType &e);
